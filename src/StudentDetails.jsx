68749bc27b0fa37e98b61789057d0fc16dedd6d7
Jayavenkate <jayajaikithuja@gmail.com>
2023-05-10 21:03:47 +0300

    crud

--- src/StudentDetails.jsx
+++ src/StudentDetails.jsx
@@ -1,16 +1,28 @@
 import { Card } from "@mui/material";
-
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 export function StudentDetails({ data, editdata, deletedata }) {
   return (
     <div>
       <Card className="student-details" elevation={3}>
         <h2>Name :{data.Name}</h2>
+
         <p>Gender :{data.Gender}</p>
         <p>Age :{data.Age}</p>
         <p>Place:{data.Place}</p>
         <p>Qualification :{data.Qualification}</p>
-        <button onClick={() => editdata(data.Id)}>edit</button>
-        <button onClick={() => deletedata(data.Id)}>delete</button>
+        <div className="icon">
+          <EditIcon color="secondary" onClick={() => editdata(data.Id)}>
+            <EditIcon />
+          </EditIcon>
+          <DeleteIcon
+            className="delete"
+            color="error"
+            onClick={() => deletedata(data.Id)}
+          >
+            <DeleteIcon />
+          </DeleteIcon>
+        </div>
       </Card>
     </div>
   );
--- src/TeacherDetails.jsx
+++ src/TeacherDetails.jsx
@@ -1,16 +1,29 @@
 import { Card } from "@mui/material";
-
-export function TeacherDetails({data,editdata,deletedata}) {
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+export function TeacherDetails({ data, editdata, deletedata }) {
   return (
     <div>
       <Card className="student-details" elevation={3}>
         <h2>Name :{data.Name}</h2>
+        
+
         <p>Gender :{data.Gender}</p>
         <p>Age :{data.Age}</p>
         <p>Place:{data.Place}</p>
         <p>Qualification :{data.Qualification}</p>
-        <button onClick={() => editdata(data.Id)}>edit</button>
-        <button onClick={() => deletedata(data.Id)}>delete</button>
+        <div className="icon">
+          <EditIcon color="secondary" onClick={() => editdata(data.Id)}>
+            <EditIcon />
+          </EditIcon>
+          <DeleteIcon
+            className="delete"
+            color="error"
+            onClick={() => deletedata(data.Id)}
+          >
+            <DeleteIcon />
+          </DeleteIcon>
+        </div>
       </Card>
     </div>
   );
